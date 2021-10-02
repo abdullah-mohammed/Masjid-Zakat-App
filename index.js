f45c663733b2746fdcbc9471e07d3210e1481365
abdullah-mohammed <aimohammed@ucdavis.edu>
2021-10-02 14:16:11 -0700

    barebones of project

---
+++ index.js
@@ -0,0 +1,5 @@
+const express = require('express');
+const passport = require('passport');
+const cookieSession = require('cookie-session');
+const GoogleStrategy = require('passport-google-oauth20');
+
